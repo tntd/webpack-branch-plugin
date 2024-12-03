44e7ba454e99e0e0c0408e7076f63d764e6bb445
xiaofei.wang <xiaofei.wang@tongdun.cn>
2024-12-03 10:29:12 +0800

    feat: gzip解压包更换

--- cacheData.js
+++ cacheData.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const zlib = require('zlib');
+const { gzip } = require('compressing');
 
 const dirCache = {};
 
@@ -32,14 +32,27 @@ module.exports = function (proxy, { open, distPath }) {
     fs.writeFileSync(rootPath, 'module.exports = {}');
   }
 
-  const saveEntry = (err, url) => {
+  const saveEntry = (err, url, fileType = '') => {
     if (err) {
       console.error('保存响应数据时出错:', err);
     } else {
       if (!mockData[url]) {
-        mockData[url] = url.slice(1);
+        mockData[url] = url.slice(1) + fileType;
         fs.writeFileSync(rootPath, `module.exports = ${JSON.stringify(mockData, null, 2)}`);
       }
+    }
+  };
+
+  const saveJson = (url, body) => {
+    try {
+      const filePath = path.join(distPath, url + '.js');
+      let parseData = JSON.parse(body);
+      if (!parseData || parseData.code !== 200) return;
+      fs.writeFile(filePath, `module.exports = ${JSON.stringify(parseData, null, 2)}`, (err) => {
+        saveEntry(err, url);
+      });
+    } catch (err) {
+      console.error('保存响应数据时出错:', err);
     }
   };
 
@@ -53,42 +66,37 @@ module.exports = function (proxy, { open, distPath }) {
         let body = [];
         // 添加对压缩数据的处理
         const isGzipped = proxyRes.headers['content-encoding'] === 'gzip';
-        if (isGzipped) {
-          proxyRes.pipe(zlib.createGunzip());
-        }
 
         proxyRes.on('data', (chunk) => {
           body.push(chunk);
         });
+
         proxyRes.on('end', () => {
           body = Buffer.concat(body);
 
-          // 如果是gzip压缩的数据，需要解压
-          if (isGzipped) {
-            body = zlib.gunzipSync(body);
-          }
-
           const fileDistPath = path.join(distPath, url);
-
           if (!fs.existsSync(fileDistPath)) {
             // 创建文件夹
             mkdir(fileDistPath);
           }
 
+          // 如果是gzip压缩的数据，需要解压
+          if (isGzipped) {
+            const tmpFile = path.join(
+              distPath,
+              `tmp_${Date.now()}_${Math.random().toString(36).slice(2)}.json`
+            );
+            gzip.uncompress(body, tmpFile).then(() => {
+              const uncompressedData = fs.readFileSync(tmpFile);
+              saveJson(url, uncompressedData);
+              fs.unlinkSync(tmpFile);
+            });
+            return;
+          }
+
           if (contentType.includes('application/json')) {
             // 处理其他类型的响应，例如JSON
-            try {
-              const filePath = path.join(distPath, url + '.js');
-              fs.writeFile(
-                filePath,
-                `module.exports = ${JSON.stringify(JSON.parse(body), null, 2)}`,
-                (err) => {
-                  saveEntry(err, url);
-                }
-              );
-            } catch (err) {
-              console.error('保存响应数据时出错:', err);
-            }
+            saveJson(url, body);
           } else if (contentType.includes('application/octet-stream')) {
             const contentDisposition = proxyRes.headers['content-disposition'];
             const pathList = url.split('/').filter((i) => !!i);
